f970d8aa8acae75eead5ef3f46779914b2c104fe
jdk-dharshankumar <153739088+jdk-dharshankumar@users.noreply.github.com>
2023-12-23 19:31:33 +0530

    added function file

---
+++ _load.php
@@ -0,0 +1,13 @@
+<?
+
+function validate($password) {
+    if ($password == "ip"){
+        return true;
+    }if ($username == "sagar@we.com" and $password == "ss") {
+        return true;
+    }else{
+        return false;
+    }
+    
+}
+?>
